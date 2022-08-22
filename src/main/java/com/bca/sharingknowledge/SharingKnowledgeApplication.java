373bfc4bff6ab02bce6da8612df0db2d63422360
abed <sipayungabed@gmail.com>
2022-08-22 10:19:16 +0700

    abed - one to many exercise (publisher)

---
+++ src/main/java/com/bca/sharingknowledge/SharingKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.bca.sharingknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bca/sharingknowledge/SharingKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bca.sharingknowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
