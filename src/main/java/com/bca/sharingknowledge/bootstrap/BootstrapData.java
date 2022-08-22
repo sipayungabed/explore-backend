0f8a20898a8ffbab9cf7eb9d8785b6d83442a6dd
Kasih Handoyo <kasih_handoyo@bca.co.id>
2022-08-22 10:48:51 +0700

    first init

--- src/main/java/com/bca/sharingknowledge/bootstrap/BootstrapData.java
+++ src/main/java/com/bca/sharingknowledge/bootstrap/BootstrapData.java
@@ -25,33 +25,26 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher publisher1 = new Publisher("gramedia", "pagedangan, no 1", "tangerang", "11111", "banten");
+        publisherRepository.save(publisher1);
 
         Author author = new Author("abed", "nego");
         Book book = new Book("menulis", "12345678");
         book.getAuthor().add(author);
         author.getBooks().add(book);
-        Author author1 = new Author("abi", "gail");
-        Book book1 = new Book("membaca", "987654321");
-
-        Publisher publisher1 = new Publisher("gramedia", "pagedangan, no 1", "tangerang", "11111", "banten");
-        publisherRepository.save(publisher1);
-        publisher1.getBooks().add(book);
-        publisher1.getBooks().add(book1);
         book.setPublisher(publisher1);
-        book1.setPublisher(publisher1);
-        publisherRepository.save(publisher1);
-
-
-
-
         authorRepository.save(author);
         bookRepository.save(book);
+
+        Author author1 = new Author("abi", "gail");
+        Book book1 = new Book("membaca", "987654321");
+        publisher1.getBooks().add(book);
+        publisher1.getBooks().add(book1);
         authorRepository.save(author1);
+        book1.setPublisher(publisher1);
         bookRepository.save(book1);
 
-
         System.out.println("total jumlah book :" + bookRepository.count());
-
         System.out.println("total buku dalam publisher 1: " + publisher1.getBooks().size());
 
     }
